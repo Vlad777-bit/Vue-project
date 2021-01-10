bfd197d9584f86ae18d7c5f7d539067b90f9af93
Vlad777-bit <mega.nozhkin@mail.ru>
2021-01-10 18:47:33 +0300

    добавлены всплывающие ошибки

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -103,7 +103,7 @@ export default {
     agree: {checked: v => v},
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -114,9 +114,11 @@ export default {
         password: this.password,
         name: this.name,
       }
-      console.log(formData)
 
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
