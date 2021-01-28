28542dfe3d3901d3b68aea241acf2051f28d28cc
Vlad777-bit <mega.nozhkin@mail.ru>
2021-01-28 17:30:15 +0300

    реализована страница Detail.vue

--- src/store/record.js
+++ src/store/record.js
@@ -12,14 +12,24 @@ export default {
       }
     },
     async fetchRecords({ dispatch, commit }) {
-        try {
-          const uid = await dispatch('getUid')
-          const records = (await firebase.database().ref(`/users/${uid}/records`).once('value')).val() || {}
-          return Object.keys(records).map(key => ({ ...records[key], id: key }))
-        } catch (e) {
-          commit('setError', e)
-          throw e
-        }
-    }
+      try {
+        const uid = await dispatch('getUid')
+        const records = (await firebase.database().ref(`/users/${uid}/records`).once('value')).val() || {}
+        return Object.keys(records).map(key => ({ ...records[key], id: key }))
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
+    async fetchRecordById({ dispatch, commit }, id) {
+      try {
+        const uid = await dispatch('getUid')
+        const record = (await firebase.database().ref(`/users/${uid}/records`).child(id).once('value')).val() || {}
+        return {...record, id}
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
   }
 }
